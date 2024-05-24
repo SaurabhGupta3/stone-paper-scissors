bfa4f8ad9f6c89e58539d387f6d87d138488b849
Saurabh Gupta <sggupta548@gmail.com>
2024-05-25 02:03:37 +0530

    getHumanChoice function added

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
-    let a = Math.floor((Math.random()*3)+1);
+    let a = Math.ceil((Math.random()*3));
     if(a == 1) {
         console.log("rock");
     } else if(a == 2) {
         console.log("paper");
-    } else {
+    } else if(a == 3) {
         console.log("scissors");
+    } else {
+        console.log("got");
     }
 }
 getComputerChoice();
+
+function getHumanChoice() {
+    let b = prompt("Enter a choice as rock or paper or scissors ");
+    console.log(b);
+}
+getHumanChoice();
